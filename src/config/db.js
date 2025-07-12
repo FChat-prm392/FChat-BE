3088c95502a2c42a45eb40a2939a6647c89f472b
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-07-12 21:49:35 +0700

    upodate

--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,16 @@
-require('dotenv').config();
-const {MongoClient} = require('mongodb');
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+const mongoose = require('mongoose');
 
-let db;
-
-async function connectDb(){
-    try{
-        await client.connect();
-        db = client.db();
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        throw error;
-        process.exit(1);
-    }
+async function connectDb() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(' Connected to MongoDB via Mongoose');
+  } catch (error) {
+    console.error(' Error connecting to MongoDB:', error);
+    throw error;
+  }
 }
 
-function getDb() {
-    if (!db) {
-        throw new Error('Database not initialized. Call connectDb first.');
-    }
-    return db;
-}
-
-module.exports = { connectDb, getDb };
+module.exports = { connectDb };
